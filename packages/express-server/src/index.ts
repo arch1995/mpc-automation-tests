a625d5fd9e0957b626115d658d39e2fc482716a4
Archit <arch1995@gmail.com>
2022-12-14 18:19:21 +0800

    changes

--- packages/express-server/src/index.ts
+++ packages/express-server/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./server";
 
 // Start the application by listening to specific port
-const port = Number(8080);
+const port = Number(80);
 app.listen(port, () => {
   console.info(`Express application started on port: ${port}`);
 });
